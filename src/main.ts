ce43cbc2f5f19893691ac2b23574fbbc1359359c
Nuno Góis <github@nunogois.com>
2025-02-13 12:53:59 +0000

    refactor: revert template variable

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core'
+import { Component, computed, signal } from '@angular/core'
 import { bootstrapApplication } from '@angular/platform-browser'
 // biome-ignore lint/style/useImportType: Angular needs this to not be a type-only import
 import { UnleashService } from './app/unleash.service'
@@ -6,17 +6,19 @@ import { UnleashService } from './app/unleash.service'
 @Component({
   selector: 'app-root',
   template: `
-    @if (!client.isReady()) {
+    @if (loading()) {
     <p>Loading...</p>
-    } @else { @let isEnabled = client.isEnabled('unleash-angular-demo');
-    <p [style.color]="isEnabled ? 'green' : 'red'">
-      {{ isEnabled ? 'Feature is enabled!' : 'Feature is disabled.' }}
+    } @else {
+    <p [style.color]="enabled() ? 'green' : 'red'">
+      {{ enabled() ? 'Feature is enabled!' : 'Feature is disabled.' }}
     </p>
     }
   `
 })
 export class PlaygroundComponent {
   client = this.unleashService.getClient()
+  loading = () => !this.client.isReady()
+  enabled = () => this.client.isEnabled('unleash-angular-demo')
 
   constructor(private unleashService: UnleashService) {}
 }
